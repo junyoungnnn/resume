3e11143750adf72e2ebd2b25da67b4857d5c8a78
Jun-Young Noh <119721103+junyoungnnn@users.noreply.github.com>
2025-01-20 00:41:47 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,310 @@
 function App() {
   return (
-    <div>
-      <h1>hello</h1>
+    <div class="container">
+      <section>
+        <h1>
+          반갑습니다,
+          <br />
+          저는 노준영입니다<span class="period-mark">.</span>
+        </h1>
+        <p class="big-paragraph">저는 자기개발을 꾸준히 하는 개발자입니다.</p>
+        <div class="other">
+          <h3>
+            부경대학교<span class="period-mark">.</span>
+          </h3>
+          <span class="role">컴퓨터인공지능공학부</span>
+          <span class="time">
+            <time datetime="2019-03">2019. 03.</time> -
+            <time datetime="2025-02">2025. 02.</time>
+          </span>
+          <p>학점 4.02 / 4.5</p>
+          <p>
+            예시: 컴퓨터소프트웨어공학과에서 4년간 공부하며 컴퓨터구조,
+            자료구조, 컴퓨터네트워크, 웹 프로그래밍 등의 과목을 우수한 성적으로
+            이수하였습니다.
+          </p>
+          <br></br>
+          <h3>
+            부산중앙고등학교<span class="period-mark">.</span>
+          </h3>
+          <span class="time">
+            <time datetime="2016-03">2016. 03.</time> -
+            <time datetime="2019-02">2019. 02.</time>
+          </span>
+        </div>
+      </section>
+      <section>
+        <h2>
+          Work Experience<span class="period-mark">.</span>
+        </h2>
+        <div class="row">
+          <div class="row-left">
+            <h3>
+              KT AIVLE SCHOOL 6기 DX TRACK<span class="period-mark">.</span>
+            </h3>
+            <span class="role">Data Analyst</span>
+            <span>
+              {" "}
+              <time datetime="2024-09">2024. 09</time> - 2025. 02{" "}
+            </span>
+          </div>
+          <div class="row-right">
+            <div class="project">
+              <h4>
+                부산 음주운전 예측 ~~~<span class="period-mark">.</span>
+              </h4>
+              <span class="time">
+                <time datetime="2024-12">2024. 12</time> - 2025. 02
+              </span>
+              <h5>
+                Description<span class="period-mark">.</span>
+              </h5>
+              <p>부산음주운전건수예측</p>
+              <h5>
+                What did I do<span class="period-mark">.</span>
+              </h5>
+              <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+              </ul>
+              <h5>
+                Tech Stack<span class="period-mark">.</span>
+              </h5>
+              <p>Keras?</p>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="row-left">
+            <h3>
+              정보 및 데이터베이스 시스템 연구실
+              <span class="period-mark">.</span>
+            </h3>
+            <span class="role">학부생 인턴</span>
+            <span>
+              {" "}
+              <time datetime="2023-08">2023. 08</time> - 2024.08
+            </span>
+          </div>
+          <div class="row-right">
+            <div class="project">
+              <h4>
+                Growth Silo<span class="period-mark">.</span>
+              </h4>
+              <span class="time">
+                <time datetime="20xx-0x">20xx. 0x</time> -
+                <time datetime="20xx-0x">20xx. 0x</time>
+              </span>
+              <h5>
+                Description<span class="period-mark">.</span>
+              </h5>
+              <p>?</p>
+              <h5>
+                What did I do<span class="period-mark">.</span>
+              </h5>
+              <ul>
+                <li>1</li>
+                <li>2</li>
+              </ul>
+              <h5>
+                Tech Stack<span class="period-mark">.</span>
+              </h5>
+              <p></p>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="row-left">
+            <h3>
+              부경대학교 개발동아리 WAP<span class="period-mark">.</span>
+            </h3>
+            <span class="role">Software Engineer</span>
+            <span>
+              <time datetime="2024-03">2024. 03</time> -
+              <time datetime="2024-06">2024. 06</time>
+            </span>
+          </div>
+          <div class="row-right">
+            <div class="project">
+              <h4>
+                Cat Town<span class="period-mark">.</span>
+              </h4>
+              <span class="time">
+                <time datetime="2024-03">2024. 03</time> -
+                <time datetime="2024-06">2024. 06</time>
+              </span>
+              <h5>
+                Description<span class="period-mark">.</span>
+              </h5>
+              <p>?</p>
+              <h5>
+                What did I do<span class="period-mark">.</span>
+              </h5>
+              <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+              </ul>
+              <h5>
+                Tech Stack<span class="period-mark">.</span>
+              </h5>
+              <p>Unity</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <h2>
+          Certificate<span class="period-mark">.</span>
+        </h2>
+        <ul className="certificate-list">
+          <li className="certificate-item">
+            <h4 className="certificate-title">정보처리기사</h4>
+            <h5 className="certificate-details">2024.12 한국산업인력공단</h5>
+          </li>
+          <li className="certificate-item">
+            <h4 className="certificate-title">SQLD</h4>
+            <h5 className="certificate-details">
+              2024.12 한국데이터산업진흥원
+            </h5>
+          </li>
+          <li className="certificate-item">
+            <h4 className="certificate-title">ADsP</h4>
+            <h5 className="certificate-details">
+              2024.11 한국데이터산업진흥원
+            </h5>
+          </li>
+          <li className="certificate-item">
+            <h4 className="certificate-title">DSAC 2급(전문가)</h4>
+            <h5 className="certificate-details">2024.10 한국생산성본부</h5>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h2>
+          Other Experiences<span class="period-mark">.</span>
+        </h2>
+        <div class="other">
+          <h3>
+            청소년 데이터 분석·활용 공모전<span class="period-mark">.</span>
+          </h3>
+          <span class="role">Data Analyst</span>
+          <span class="time">
+            <time datetime="2024-11">2024. 11.</time> - 2024.12
+          </span>
+          <p>?</p>
+        </div>
+        <div class="other">
+          <h3>
+            DIVE 2024<span class="period-mark">.</span>
+          </h3>
+          <span class="role">Data Analyst</span>
+          <span class="time">
+            <time datetime="2024-10-04">2017. 10. 03.</time> -
+            <time datetime="2024-10-06">2024. 10. 06.</time>
+          </span>
+          <p>?</p>
+        </div>
+        <div class="other">
+          <h3>
+            Blog<span class="period-mark">.</span>
+          </h3>
+          <span class="time">
+            <time datetime="2024-09">2024. 09.</time> - 현재
+          </span>
+          <p>
+            <a href="https://velog.io/@junyoung0123/posts" target="_blank">
+              벨로그
+            </a>
+            ?
+            <a href="https://blog.naver.com/njy3006" target="_blank">
+              네이버 블로그
+            </a>
+            이라는 이름의 블로그를 운영했습니다.
+          </p>
+        </div>
+      </section>
+      <section>
+        <h2>
+          Skills<span class="period-mark">.</span>
+        </h2>
+        <div class="other">
+          <h3>
+            Overall<span class="period-mark">.</span>
+          </h3>
+          <ul>
+            <li>
+              아름다운 유저 인터페이스 및 미려한 애니메이션 구현을 좋아합니다.
+            </li>
+            <li>
+              항상 최신의 기술이 옳다고 여기지 않습니다. 상황에 따라 적절한
+              선택이 있다고 믿습니다.
+            </li>
+            <li>
+              업무에 필요하다면 능숙한 분야가 아니더라도 적극적으로 탐색하여
+              최적의 결과를 낼 수 있도록 노력합니다.
+            </li>
+            <li>
+              회사 혹은 팀의 프로세스 및 문화를 개선하거나 바꾸려는 시도를
+              적극적으로 합니다.
+            </li>
+          </ul>
+        </div>
+        <div class="other">
+          <h3>
+            Communication<span class="period-mark">.</span>
+          </h3>
+          <ul>
+            <li>직위 및 포지션에 관계없이 적극적으로 생각을 표현합니다.</li>
+            <li>지적 겸손함을 유지하기 위해 노력합니다.</li>
+            <li>커뮤니케이션은 적은 것보다는 많은게 좋다고 믿습니다.</li>
+          </ul>
+        </div>
+
+        <div class="other">
+          <h3>
+            React<span class="period-mark">.</span>
+          </h3>
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+          </ul>
+        </div>
+      </section>
+      <section>
+        <h2>
+          Contact<span class="period-mark">.</span>
+        </h2>
+        <ul>
+          <li>
+            깃허브:
+            <a href="https://github.com/junyoungnnn" target="_blank">
+              {" "}
+              https://github.com/junyoungnnn
+            </a>
+          </li>
+          <li>
+            블로그:
+            <a href="https://velog.io/@junyoung0123/posts" target="_blank">
+              {" "}
+              https://velog.io/@junyoung0123/posts
+            </a>
+          </li>
+          <li>
+            이전 블로그:
+            <a href="https://blog.naver.com/njy3006" target="_blank">
+              {" "}
+              https://blog.naver.com/njy3006
+            </a>
+          </li>
+        </ul>
+      </section>
     </div>
   );
 }
